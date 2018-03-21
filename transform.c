f2ea8e522747d61d6f730be8fd429a9b34129132
Duncuiad <36989446+Duncuiad@users.noreply.github.com>
2018-03-21 08:52:24 -0500

    Create transform.c

---
+++ transform.c
@@ -0,0 +1,11 @@
+#include transform.h
+
+//Options and description listed in the header
+Index transform(Index ind, char option){
+//STRONZO, FINISCI QUESTO PRIMA DI COMPILARE
+}
+
+// Apply a certain transformation to a grid
+void transpose(Grid *g);
+void reflectHor(Grid *g);
+void reflectVer(Grif *g);
